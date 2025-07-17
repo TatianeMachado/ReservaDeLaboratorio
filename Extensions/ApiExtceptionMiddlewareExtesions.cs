a7d4d6ac26ec8a975f3a6f31017b0564509c2d49
Tatine Machado <tatiane.nandam@gmail.com>
2025-07-17 19:08:34 -0300

    Tratamento de  exceções e config adicionais

---
+++ Extensions/ApiExtceptionMiddlewareExtesions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace ReservaDeLaboratorio.Extensions;
+
+public static class ApiExtceptionMiddlewareExtesions
+{
+    public static void ConfigureApiExceptionMiddleware(this IApplicationBuilder app)
+{
+    app.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if (contextFeature?.Error != null)
+                {
+                    var errorDetails = new
+                    {
+                        Status = context.Response.StatusCode,
+                        Message =contextFeature.Error.Message,
+                        Trace = contextFeature.Error.StackTrace
+                    };
+                    await context.Response.WriteAsJsonAsync(errorDetails);
+                }
+            });
+        }
+        );
+        
+}
+}
---
+++ Filters/ApiLogginFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ReservaDeLaboratorio.Filters;
+
+public class ApiLogginFilter : IActionFilter
+{
+
+    private readonly ILogger<ApiLogginFilter> _logger;
+    public ApiLogginFilter(ILogger<ApiLogginFilter> logger)
+    {
+        _logger = logger;
+
+    }
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        // Logica antes da execucao do metodo
+        _logger.LogInformation("### Executando -> OnActionExecuting");
+        _logger.LogInformation("#####################################");
+        _logger.LogInformation($"{DateTime.Now.ToLongTimeString()}");
+        _logger.LogInformation("#####################################");
+        _logger.LogInformation($"ModelState: {context.ModelState.IsValid}");
+        _logger.LogInformation("#####################################");
+
+
+    }
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        // Logica depois da execucao do metodo
+        _logger.LogInformation("### Executando -> OnActionExecuted");
+        _logger.LogInformation("#####################################");
+        _logger.LogInformation($"{DateTime.Now.ToLongTimeString()}");
+        _logger.LogInformation($"Status Code: {context.HttpContext.Response.StatusCode}");
+        _logger.LogInformation("#####################################");
+
+    }
+}
